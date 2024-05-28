f5a54c906930394828c9baa573ca3484da814146
paigauresh <paigauresh@gmail.com>
2024-05-29 00:07:50 +0530

    front end draft 1 completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Navbar from "../components/Navbar"
-import '../public/styles/globals.css'
+import Navbar from "../components/Navbar";
+import '../public/styles/globals.css';
 import Footer from "@/components/Footer";
 import About from "@/components/About";
 import Education from "@/components/Education";
@@ -11,13 +11,17 @@ import Contact from "@/components/Contact";
 export default function Home() {
   return (
     <main>
-      <Navbar/>
-        <div id="about"><About /></div>
-        <div id="education"><Education/></div>
-        <div id="certificates"><Certificates/></div>
-        <div id="skills"><Skills/></div>
-        <div id="contact"><Contact/></div>
-      <Footer/>
+      <Navbar />
+      <div id="about"><About /></div>
+      <hr />
+      <div id="education"><Education /></div>
+      <hr />
+      <div id="certificates"><Certificates /></div>
+      <hr />
+      <div id="skills"><Skills /></div>
+      <hr />
+      <div id="contact"><Contact /></div>
+      <Footer />
     </main>
   );
 }
