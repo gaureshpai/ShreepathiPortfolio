babbdb2e72ffc8711eb5fc1c12d6183946a7638d
paigauresh <paigauresh@gmail.com>
2024-07-05 21:41:03 +0530

    number of messages returned as an alert

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: [
-        'github.com'
-      ],
       remotePatterns: [
         {
           protocol: 'https',
