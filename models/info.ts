70588ce93a491b9e20efd23894d6485361176940
paigauresh <paigauresh@gmail.com>
2024-09-17 19:20:41 +0530

    mongodb uri to .env

--- models/info.ts
+++ models/info.ts
@@ -1,12 +1,13 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://paigauresh:shreepathishree7@cluster0.7blkaad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+const client = new MongoClient(process.env.MONGODB_URL);
+
 export async function getSkills() {
     try {
         const database = client.db('info');
         const collection = database.collection('skills');
-        const skills = await collection.find({}).toArray();
-        return skills;
+        const skills = await collection.find({});
+
+        return JSON.stringify(skills) || [];
     } catch (error) {
         console.error("Error fetching skills:", error);
         return null;
@@ -17,8 +18,9 @@ export async function getAbout() {
     try {
         const database = client.db('info');
         const collection = database.collection('about');
-        const about = await collection.find({}).toArray();
-        return about;
+        const about = await collection.find({});
+
+        return JSON.stringify(about) || [];
     } catch (error) {
         console.error("Error fetching about:", error);
         return null;
@@ -29,8 +31,9 @@ export async function getEducation() {
     try {
         const database = client.db('info');
         const collection = database.collection('education');
-        const education = await collection.find({}).toArray();
-        return education;
+        const education = await collection.find({});
+
+        return JSON.stringify(education) || [];
     } catch (error) {
         console.error("Error fetching education:", error);
         return null;
@@ -41,8 +44,9 @@ export async function getCerts() {
     try {
         const database = client.db('info');
         const collection = database.collection('certs');
-        const certs = await collection.find({}).toArray();
-        return certs;
+        const certs = await collection.find({});
+
+        return JSON.stringify(certs) || [];
     } catch (error) {
         console.error("Error fetching certs:", error);
         return null;
